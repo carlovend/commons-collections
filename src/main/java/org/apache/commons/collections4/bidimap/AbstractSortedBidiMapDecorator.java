ddf186645809e51db13213641e9bf0cb5ee216c5
PaoloCarmine1201 <114732828+PaoloCarmine1201@users.noreply.github.com>
2023-11-28 16:15:56 +0100

    fix an error

--- src/main/java/org/apache/commons/collections4/bidimap/AbstractSortedBidiMapDecorator.java
+++ src/main/java/org/apache/commons/collections4/bidimap/AbstractSortedBidiMapDecorator.java
@@ -48,7 +48,7 @@ public abstract class AbstractSortedBidiMapDecorator<K, V>
      * @param map  the map to decorate, must not be null
      * @throws NullPointerException if the collection is null
      */
-    protected AbstractSortedBidiMapDecorator(final SortedBidiMap<K, V> map) {
+    public AbstractSortedBidiMapDecorator(final SortedBidiMap<K, V> map) {
         super(map);
     }
 
