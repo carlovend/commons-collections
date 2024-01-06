de70d850d5d333842d8192fcdf050bf057334b76
alessaless <zoccolaaless@gmail.com>
2024-01-06 11:06:21 +0100

    Microbenchmarking with JMH

--- src/main/java/org/apache/commons/collections4/set/PredicatedNavigableSet.java
+++ src/main/java/org/apache/commons/collections4/set/PredicatedNavigableSet.java
@@ -1,4 +1,4 @@
-/*
+package org.apache.commons.collections4.set;/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.commons.collections4.set;
+
 
 import java.util.Iterator;
 import java.util.NavigableSet;
